0219d4cf3089b23d812f2694b108522a774f181f
laserdi <v013v@ya.ru>
2023-01-25 21:41:08 +0200

    2023.01.25 From 3750317d

--- src/main/java/ru/yandex/practicum/ShareItApp.java
+++ src/main/java/ru/yandex/practicum/ShareItApp.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//2023.01.25 From 3750317d
 @SpringBootApplication
 public class ShareItApp {
     public static void main(String[] args) {
